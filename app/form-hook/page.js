4df5243fc15cc9cf6763444bc7f84fcd1995f6c1
macdonst <simon.macdonald@gmail.com>
2023-08-14 11:50:51 -0400

    Postal Code pattern
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/form-hook/page.js
+++ app/form-hook/page.js
@@ -21,7 +21,7 @@ export default function App() {
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
         First Name
       </label>
-      <input type="text" placeholder="First name" className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" {...register("firstName", {required: true, maxLength: 80})} />
+      <input type="text" placeholder="First name" className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" {...register("firstName", {required: true })} />
       {errors.firstName?.type === "required" && (
         <p className="mb-4 text-gray-700" role="alert">First name is required</p>
       )}
@@ -29,7 +29,7 @@ export default function App() {
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
         Last Name
       </label>
-      <input type="text" placeholder="Last name" className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" {...register("lastName", {required: true, maxLength: 100})} />
+      <input type="text" placeholder="Last name" className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" {...register("lastName", {required: true })} />
       {errors.lastName?.type === "required" && (
         <p className="mb-4 text-gray-700" role="alert">Last name is required</p>
       )}
@@ -55,7 +55,7 @@ export default function App() {
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="province">
         Province
       </label>
-      <input type="text" placeholder="Province" className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" {...register("province", {required: true, maxLength: 12})} autoComplete="address-level1"/>
+      <input type="text" placeholder="Province" className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" {...register("province", {required: true })} autoComplete="address-level1"/>
       {errors.province?.type === "required" && (
         <p className="mb-4 text-gray-700" role="alert">Province is required</p>
       )}
@@ -63,9 +63,12 @@ export default function App() {
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="postalCode">
         Postal Code
       </label>
-      <input type="text" placeholder="Postal Code" className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" {...register("postalCode", {required: true})} autoComplete="postal-code"/>
+      <input type="text" placeholder="Postal Code" className="mb-4 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" {...register("postalCode", {required: true, pattern: /^[ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z] [0-9][ABCEGHJ-NPRSTV-Z][0-9]$/i })} autoComplete="postal-code"/>
       {errors.postalCode?.type === "required" && (
         <p className="mb-4 text-gray-700" role="alert">Postal code is required</p>
+      )}
+      {errors.postalCode?.type === "pattern" && (
+        <p className="mb-4 text-gray-700" role="alert">Invalid Postal code</p>
       )}
 
       <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" />
